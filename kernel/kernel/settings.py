23153827170b43b4d6aa8d4148eecbdd4c7bd962
sageteam <simplewordkill@gmail.com>
2019-03-29 11:58:22 +0430

    settings urls

--- kernel/kernel/settings.py
+++ kernel/kernel/settings.py
@@ -138,3 +138,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/upload/')
+
+MEDIA_URL = '/media/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'media/static/'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'media/collectstatic')
+
+LOGIN_REDIRECT_USER = "signin"
+LOGIN_URL = "signin"
